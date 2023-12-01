96a7bb78cdc7c09ced7c951da09c7bc310486902
Minh Quyen Doan <doan.minh.quyen22201@gmail.com>
2023-12-01 21:48:10 +0700

    fix front-end wrong basequery

--- client/src/features/api/apiSlice.js
+++ client/src/features/api/apiSlice.js
@@ -3,8 +3,8 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    // baseUrl: "https://wbsach-api.vercel.app",
-    baseUrl: "http://127.0.0.1:3000",
+    baseUrl: "https://wbsach-api.vercel.app",
+    //baseUrl: "http://127.0.0.1:3000",
     prepareHeaders: (headers, { getState }) => {
       const token = getState().auth.token;
 
